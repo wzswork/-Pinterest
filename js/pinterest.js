37d8a0d47ddc97f7c6b5db222b5dcd9e31b11de6
王阿石 <wzswork@hotmail.com>
2017-02-08 08:46:22 +0800

    基础

---
+++ js/pinterest.js
@@ -0,0 +1,42 @@
+var pinterest = {
+	// 图片地址
+	imgUrls: [],
+	// 列数
+	colNum: 0,
+	// 默认参数
+	default: {
+		maxW: 300, //每列最大宽度
+		minW: 200,  //每列最小宽度
+		height: 300, //图片高度
+		interval: 30, //m每列间隔
+	}
+	//初始化
+	init: function (){
+
+	},
+	// 添加初始图片
+	creat: function(){
+
+	}
+	//在末尾添加图片
+	appendImg: function(){
+
+	},
+	// 监测滚动状态
+	scrollListen: function(){
+
+	},
+	// 检测哪一列需要添加
+	checkColumn: function(){
+
+	},
+	// 根据浏览器大小分列数
+	divide: function(){
+
+	},
+	// 监测浏览器大小变化
+	resize: function(){
+
+	}
+
+}
