dbd1fd5adde2fff51eadb7e2b989b35eb6663d38
王阿石 <wzswork@hotmail.com>
2017-02-25 15:10:22 +0800

    1.0

--- js/pinterest.js
+++ js/pinterest.js
@@ -7,19 +7,24 @@ var pinterest = {
 	imgIndex: 0,
 	// 列数
 	col: [],
+	// 
+	scrollTop : 0,
 	// 默认参数
 	default: {
 		maxW: 300, //每列最大宽度
 		minW: 200,  //每列最小宽度
 		height: 300, //图片高度
-		interval: 30, //m每列间隔
+		interval: 30, //每列间隔
 	},
 	//初始化
 	init: function(){
 		var _page = this;
+		_page.imgIndex = 0;
 		_page.container.innerHTML = '';
 		_page.divide();
 		_page.create();
+		_page.scrollListen();
+		_page.resize();
 	},
 	// 添加初始图片
 	create: function(){
@@ -37,12 +42,20 @@ var pinterest = {
 		}
 		for(i=0; i<_page.default.column; i++){
 			_page.col[i] = document.createElement("div");
+			// _page.col[i].style.margin = "0 "+_page.default.interval+"px";
 			_page.appendImg(_page.col[i], i);
 			_page.container.appendChild(_page.col[i]);
 		}
-		_page.checkColumn();
+		for (var start = 0; start < _page.col.length; start++) {
+			var eleColumn = _page.col[start];
+			if (eleColumn) {
+				if (eleColumn.offsetTop + eleColumn.clientHeight < this.scrollTop + (window.innerHeight || document.documentElement.clientHeight)) {
+					_page.checkColumn();
+				}
+			}		
+		}
 	},
-	//在添加图片
+	//添加图片
 	appendImg: function(dom){
 		var _page = this;
 		var imgBox = document.createElement("div");
@@ -50,50 +63,54 @@ var pinterest = {
 			return;
 		}
 		imgBox.className = "imgBox";
-		imgBox.innerHTML = '<img width='+(_page.default.width - 32)+' src = '+_page.imgUrls[_page.imgIndex]+'><p>'+_page.imgIndex+'<p>';
+		imgBox.innerHTML = '<img width='+(_page.default.width - 38)+' src = '+_page.imgUrls[_page.imgIndex]+'><p>'+_page.imgIndex+'<p>';
 		_page.imgIndex++;
 		dom.appendChild(imgBox);
 	},
 	// 监测滚动状态
 	scrollListen: function(){
-
+		var _page = this;
+		window.onscroll = function(){
+			_page.scrollTop = _page.scrollTop || document.documentElement.scrollTop || document.body.scrollTop;
+			var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+			if(Math.abs(scrollTop - _page.scrollTop) > 100){
+				_page.scrollTop = scrollTop;
+				_page.checkColumn();
+			}
+		}
 	},
 	// 检测哪一列需要添加
 	checkColumn: function(){
 		var _page = this;
-		var scrollH = document.body.scrollHeight;
-		var flag = 0;
-		while(flag != _page.col.length){
-			for(var i = 0; i < _page.col.length; i++){
-				console.log(_page.col[i].lastChild.getBoundingClientRect().bottom);
-		    	console.log(document.body.scrollHeight);
-				if(_page.col[i].lastChild.getBoundingClientRect().bottom < document.body.scrollHeight){
-					_page.appendImg(_page.col[i])
-				}else{
-					flag++
+		for (var start = 0; start < _page.col.length; start++) {
+			var eleColumn = _page.col[start];
+			if (eleColumn) {
+				if (eleColumn.offsetTop + eleColumn.clientHeight < this.scrollTop + (window.innerHeight || document.documentElement.clientHeight)) {
+					_page.appendImg(eleColumn);
 				}
-			}
-			flag = (flag == _page.col.length) ? flag : 0;
+			}		
 		}
 		
-
 	},
 	// 根据浏览器大小分列数
 	divide: function(){
 		var _page = this;
 		var opts = _page.default;
-		var width = document.body.clientWidth;
+		var width = document.documentElement.clientWidth || document.body.clientWidth;
 		var column = 1, columnW = 0;
 		while(!(width / column <= 300 && width / column >= 200)){
 			column++
 		}
 		columnW = width / column;
-		opts.column = column;
-		opts.width = columnW;
+		opts.column = Math.floor(column);
+		opts.width = Math.floor(columnW);
 	},
 	// 监测浏览器大小变化
 	resize: function(){
-
+		var _page = this;
+		window.onresize = function(){
+			_page.init();
+		}
 	}
 
 }
